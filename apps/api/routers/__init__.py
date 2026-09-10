39aac2942410953356d69632d39debb523f9f4dd
Anuj Mundu <anujmark.edwin.ame@gmail.com>
2026-09-10 19:20:25 +0530

    feat(api): export mlops_router in routers package

--- apps/api/routers/__init__.py
+++ apps/api/routers/__init__.py
@@ -1,13 +1,14 @@
-from apps.api.routers.health import router as health_router
+from apps.api.routers.agents import router as agents_router
 from apps.api.routers.auth import router as auth_router
-from apps.api.routers.projects import router as projects_router
 from apps.api.routers.datasets import router as datasets_router
 from apps.api.routers.experiments import router as experiments_router
+from apps.api.routers.health import router as health_router
 from apps.api.routers.ml import router as ml_router
-from apps.api.routers.vision import router as vision_router
+from apps.api.routers.mlops import router as mlops_router
 from apps.api.routers.nlp import router as nlp_router
+from apps.api.routers.projects import router as projects_router
 from apps.api.routers.rag import router as rag_router
-from apps.api.routers.agents import router as agents_router
+from apps.api.routers.vision import router as vision_router
 
 __all__ = [
     "health_router",
@@ -20,4 +21,5 @@ __all__ = [
     "nlp_router",
     "rag_router",
     "agents_router",
+    "mlops_router",
 ]
